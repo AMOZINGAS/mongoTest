9649df184879ba1a834d5a888d243506438b94fb
Amós Olguin <PC@DESKTOP-S02NUEJ>
2024-04-20 01:32:38 -0600

    Modificaciones

--- src/main/java/DAO/PersonDAO.java
+++ src/main/java/DAO/PersonDAO.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.Block;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
@@ -37,17 +38,27 @@ public class PersonDAO {
         CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
         ConnectionString stringConnection = new ConnectionString("mongodb://localhost/27017");
         MongoClientSettings clientsSettings = MongoClientSettings.builder().applyConnectionString(stringConnection).codecRegistry(codecRegistry).build();
+        //Aqui usamos el MongoClient para poder crear las conexiones en la base de datos de mongo
         MongoClient dbServer = MongoClients.create(clientsSettings);
+        //Inicializamos a la base de datos con el dbserver que es la conexion a mongo y ahora a la base de nosotros
         this.dataBase = dbServer.getDatabase("AmosHeli");
+        //Mas abajo podemos crear la conexion de una vez si vamos a utilizar los
+        //POJOs como conexion de collections
         this.collection = dataBase.getCollection("person", Person.class);
         
     }
     
     public void readPersonCollection(){
         
+        //Podemos crear nuestra propia conexion con el documento con un 
+        //documento un poco abstracto
         MongoCollection<Document> collectionDocument = dataBase.getCollection("person");
+        //el mongo cusros lo usamos para iterar sobre los datos que tiene esa
+        //coleccion de la base de datos
         try (MongoCursor<Document> cursor = collectionDocument.find().iterator()) {
             while (cursor.hasNext()) {
+                //es donde pondremos nuestro dato con la informacion de la base 
+                //de datos de mongo
                 Document doc = cursor.next();
                 System.out.println(doc.toJson());
             }
